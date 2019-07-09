e54835078598f172e6b6e73fa31449aed5d644d1
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-09 22:51:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ def introduction_with_language(name, language)
   puts message
 end
 
-language = "Ruby"
+language = "Ruby
 def introduction_with_language_optional(name, language)
   message = "Hi, my name is #{name} and I am learning to program in #{language}."
   puts message
